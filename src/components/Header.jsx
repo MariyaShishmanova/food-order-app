18afb27f28c542e56f44f53c6cd8407f212e62d8
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-07-02 11:28:25 +0300

    created Header, Meals, MealItem and Button components

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import logo from '../assets/logo.jpg';
+import Button from './UI/Button';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logo} alt="restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+
+      <nav>
+        <Button textOnly>Cart (0)</Button>
+      </nav>
+    </header>
+  );
+}
