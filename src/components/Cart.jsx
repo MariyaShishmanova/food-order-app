28bf573f475aad51a073b565f5a61f7bd2acf719
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-07-04 10:46:50 +0300

    checkout component created

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import Modal from './UI/Modal';
 import CartContext from '../store/CartContext';
-import CartItem from './UI/CartItem';
+import CartItem from './CartItem';
 import UserProgressContext from '../store/UserProgressContext';
 import { currencyFormatter } from '../util/formatting';
 import Button from './UI/Button';
@@ -16,8 +16,12 @@ export default function Cart() {
     userProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal className="cart" open={userProgressCtx.progress === 'cart'} onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}>
       <h2>Your Cart</h2>
 
       <ul>
@@ -32,7 +36,7 @@ export default function Cart() {
         <Button textOnly onClick={handleCloseCart}>
           Close
         </Button>
-        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+        {cartCtx.items.length > 0 && <Button onClick={handleGoToCheckout}>Go to Checkout</Button>}
       </p>
     </Modal>
   );
